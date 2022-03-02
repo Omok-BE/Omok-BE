cf36f16cf131dfb7137216a55194b352445bfe33
haksae90 <95196634+Haksae90@users.noreply.github.com>
2022-03-02 19:54:47 +0900

    (feat) socket

--- socket.js
+++ socket.js
@@ -1,5 +1,5 @@
 const app = require("./app");
-// const { users } = require('./models')
+const Users = require('./models/users')
 
 const httpServer = require("http").createServer(app);
 const { Server } = require("socket.io");
@@ -15,22 +15,24 @@ instrument(io, {
   auth: false,
 });
 
-io.on("connection", (socket) => {
+io.on("connection", async (socket) => {
     console.log("client와 연결됨 ✅");
     socket.onAny((event) => {
-      console.log(`Socket Event: ${event}`);
+      console.log(`Socket Event: ${event}`); // 발생하는 이벤트 console에 출력
 
     socket.on("nickname", (nickname) => socket["nickname"] = nickname); // 닉네임 설정
-    socket.on("enter_room", (roomName) => {                             // 방 입장시
+    socket.on("enterRoom", (roomName) => {                             // 방 입장시
         socket.join(roomName)                                           // 방 이름 지정
-        socket.to(roomName).emit("welcome", socket.nickname)            // 채팅창에 입장 알림
+        const userInfo = await Users.findOne({id: socket.nickname}, {_id: false, id: true, score: true, point: true, state: true})
+        socket.to(roomName).emit("welcome", socket.nickname, userInfo)            // 채팅창에 입장 알림
     });
-    socket.on("new_message", (msg, room) => {                           // 메시지 전송시
-        socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`); // 방에 있는 모든 사람에게 메시지 전송
+    socket.on("chat", (roomName, chat) => {                // 메시지 전송시    
+        const data = { nickname: socket.nickname, chat } 
+        socket.to(roomName).emit("chat", data); // 방에 있는 모든 사람에게 메시지 전송
     });
     
     socket.on("disconnecting", () => {                                                 // 방 퇴장시
-        socket.rooms.forEach((roos) => socket.to(room).emit("bye", socket.nickname));  // 방에 bye 전달 
+        socket.rooms.forEach((room) => socket.to(room).emit("bye", socket.nickname));  // 방에 bye 전달 
     })
 
   });
