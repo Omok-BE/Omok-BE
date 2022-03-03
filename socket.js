403d7162a14114b9648183c192de3608ad5e1811
Haksae <95196634+Haksae90@users.noreply.github.com>
2022-03-03 22:08:45 +0900

    update) cors, feat) waitingRoomSocket (#6)

--- socket.js
+++ socket.js
@@ -7,22 +7,25 @@ const { Server } = require("socket.io");
 const { instrument } = require("@socket.io/admin-ui");
 
 const io = new Server(httpServer, {
-  cors: {
-    origin: ["https://admin.socket.io"],
-    credentials: true,
-  },
+  cors : ({
+    origin: true,
+    credentials: true
+  })
 });
 instrument(io, {
   auth: false,
 });
 
+
+// 대기실 socket
+const waitingRoom = io.of('/waiting')
+let theRoom; // 접속한 방
 // 방 참가 인원 카운트 함수
-function countRoom(roomName){
-  return io.sockets.adapter.rooms.get(roomName)?.size
+function waitingRoomCount(roomName){
+  return waitingRoom.adapter.rooms.get(roomName)?.size
 }
 
-let theRoom; // 접속한 방
-io.on("connection", (socket) => {
+waitingRoom.on("connection", (socket) => {
     console.log("client와 연결됨 ✅");
     socket.onAny((event) => {
       console.log(`Socket Event: ${event}`); // 발생하는 이벤트 console에 출력
@@ -34,7 +37,7 @@ io.on("connection", (socket) => {
       theRoom = roomName;
       socket.join(roomName)                                                       // 방 이름 지정
       socket.join(state)                                                         // status 지정
-      const playerCnt = countRoom(state)                                         // 플레이어 Cnt
+      const playerCnt = waitingRoomCount(state)                                         // 플레이어 Cnt
       await Rooms.updateOne({ roomName }, { $set: { playerCnt }})                 // 방 playerCnt 업데이트
       const userInfo = await Users.findOne({ id: socket.nickname }, { _id: false, id: true, score: true, point: true, state: true })  // 유저 정보 조회
       socket.to(roomName).emit("welcome", socket.nickname, userInfo)              // 채팅창에 입장 알림
@@ -45,7 +48,7 @@ io.on("connection", (socket) => {
       const state = "observer"
       socket.join(roomName)                                                       // 방 이름 지정
       socket.join(state)                                                         // status 지정
-      const observerCnt = countRoom(state)                                         // 플레이어 Cnt
+      const observerCnt = waitingRoomCount(state)                                         // 플레이어 Cnt
       await Rooms.updateOne({ roomName }, { $set: { observerCnt }})                 // 방 playerCnt 업데이트
       const userInfo = await Users.findOne({ id: socket.nickname }, { _id: false, id: true, score: true, point: true, state: true })  // 유저 정보 조회
       socket.to(roomName).emit("welcome", socket.nickname, userInfo)              // 채팅창에 입장 알림
@@ -59,15 +62,15 @@ io.on("connection", (socket) => {
     socket.on("disconnecting", () => {                                         // 방 퇴장시
       socket.to(theRoom).emit("bye", socket.nickname)                          // 방에 bye 전달
       if(socket.rooms.has("player")){                                          // 나가는 사람이 플레이어라면
-        const playerCnt = countRoom("player") -1                               // 플레이어 한명 나가면 -1
+        const playerCnt = waitingRoomCount("player") -1                               // 플레이어 한명 나가면 -1
         await Rooms.updateOne({ roomName }, { $set: { playerCnt }})            // 실시간 업데이트
       } else {                                                                 // 나가는 사람이 관전자면
-        const observerCnt = countRoom("observer") -1
+        const observerCnt = waitingRoomCount("observer") -1
         await Rooms.updateOne({ roomName }, { $set: { observerCnt }})
       }
     })
-
   });
 });
+
 
 module.exports = { httpServer };
