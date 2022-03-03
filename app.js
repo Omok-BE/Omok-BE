e02e7c50f4fa312bec094486717e6dfa15ac6981
haksae90 <95196634+Haksae90@users.noreply.github.com>
2022-03-03 22:07:12 +0900

    update) cors, feat) waitingRoomSocket

--- app.js
+++ app.js
@@ -15,7 +15,10 @@ const requestMiddleware = (req, res, next) => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(requestMiddleware);
-app.use(cors());
+app.use(cors({
+  origin: true,
+  credentials: true
+}));
 
 app.use("/", [usersRouter]);
 
