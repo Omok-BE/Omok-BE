9db697166473088a25df4d9568822015c6a2d9d6
haksae90 <95196634+Haksae90@users.noreply.github.com>
2022-03-09 04:35:10 +0900

    comment: update socket comment

--- socket.js
+++ socket.js
@@ -17,11 +17,11 @@ instrument(io, {
 });
 
 
-// by hak, 대기실 socketIO
+// 대기실 socketIO
 const waitingRoom = io.of('/waiting')
 let theRoomNumber;
 
-// by hak, 방 인원 카운트_210304
+//방 인원 카운트_210304
 function waitingRoomCount(roomName){
   return waitingRoom.adapter.rooms.get(roomName)?.size
 }
@@ -30,9 +30,9 @@ waitingRoom.on("connection", (socket) => {
     console.log("client와 연결됨 ✅");
     socket.onAny((event) => {
       console.log(`Socket Event: ${event}`);
-    // by hak, socket nickname 설정_210303
+    //socket nickname 설정_210303
     socket.on("nickname", (nickname) => socket["nickname"] = nickname);
-    // by hak, 대기실 플레이어로 입장시 정보 업데이트_210303
+    //대기실 플레이어로 입장시 정보 업데이트_210303
     socket.on("enterRoomPlayer", async (roomNum) => {
       const state = "player"
       theRoomNumber = roomNum;
@@ -43,7 +43,7 @@ waitingRoom.on("connection", (socket) => {
       const userInfo = await Users.findOne({ id: socket.nickname }, { _id: false, id: true, score: true, point: true, state: true })
       socket.to(roomNum).emit("welcome", socket.nickname, userInfo)
     });
-    // by hak, 대기실 옵져버로 입장시 정보 업데이트_210303
+    //대기실 옵져버로 입장시 정보 업데이트_210303
     socket.on("enterRoomObserver", async (roomNum) => {
       theRoomNumber = roomNum;
       const state = "observer"
@@ -54,7 +54,7 @@ waitingRoom.on("connection", (socket) => {
       const userInfo = await Users.findOne({ id: socket.nickname }, { _id: false, id: true, score: true, point: true, state: true })
       socket.to(roomNum).emit("welcome", socket.nickname, userInfo)
     });
-    // by hak, 플레이어로 변경시 정보 업데이트_210304
+    //플레이어로 변경시 정보 업데이트_210304
     socket.on("changeToPlayer", async (player) => {
       const previousTeam = "observer"
       socket.leave(previousTeam)
@@ -64,7 +64,7 @@ waitingRoom.on("connection", (socket) => {
       await Rooms.updateMany({ roomNum: theRoomNumber }, { $set: { playerCnt, observerCnt }})
       socket.to(roomNum).emit("moveToPlayer", socket.nickname)
     })
-    // by hak, 옵져버로 변경시 정보 업데이트_210304
+    //옵져버로 변경시 정보 업데이트_210304
     socket.on("changeToObserver", async (observer) => {
       const previousTeam = "player"
       socket.leave(previousTeam)
@@ -74,12 +74,12 @@ waitingRoom.on("connection", (socket) => {
       await Rooms.updateMany({ roomNum: theRoomNumber }, { $set: { playerCnt, observerCnt }})
       socket.to(roomNum).emit("moveToObserver", socket.nickname)
     })
-    // by hak, 대기실 내 채팅_210303
+    //대기실 내 채팅_210303
     socket.on("chat", (chat) => {
         const data = { nickname: socket.nickname, chat } 
         socket.to(theRoomNumber).emit("chat", data);
     });
-    // by hak, 퇴장시 방 최신화_210304    
+    //퇴장시 방 최신화_210304    
     socket.on("disconnecting", async () => {
       socket.to(theRoomNumber).emit("bye", socket.nickname)
       if(socket.rooms.has("player")){
