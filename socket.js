8d0f9c1b0324643bfd6ce936f53ed3263cc29424
haksae90 <95196634+Haksae90@users.noreply.github.com>
2022-03-10 21:33:02 +0900

    test) waitingRoom socket

--- socket.js
+++ socket.js
@@ -98,9 +98,11 @@ waitingRoom.on("connection", (socket) => {
       waitingRoom.to(theRoomNumber).emit("bye", socket.nickname)
       if(socket.rooms.has("player")){
         const playerCnt = waitingRoomCount("player") -1
+        console.log("퇴장시 플레이", playerCnt)
         await Rooms.updateOne({ roomNum: theRoomNumber }, { $set: { playerCnt }})
       } else {
         const observerCnt = waitingRoomCount("observer") -1
+        console.log("퇴장시 관전자", observerCnt)
         await Rooms.updateOne({ roomNum: theRoomNumber }, { $set: { observerCnt }})
       }} catch(error) {
         console.log(error)
