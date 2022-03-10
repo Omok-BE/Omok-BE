31914a0223ec92a44523b5fa03b6a8cb56c6505e
haksae90 <95196634+Haksae90@users.noreply.github.com>
2022-03-10 14:35:44 +0900

    test) gameRoom socket

--- socket.js
+++ socket.js
@@ -132,7 +132,7 @@ gameRoom.on("connect", async (socket) =>{
     console.log(data)
     console.log(thisgameNum)
     console.log(socket.rooms)
-    io.to(thisgameNum).emit("chat", data);
+    gameRoom.to(thisgameNum).emit("chat", data);
     // socket.to(thisgameNum).emit("chat", data);
     });
     
