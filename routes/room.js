19105f1e6024edb43ace495714dd1f78f6df9b04
haksae90 <95196634+Haksae90@users.noreply.github.com>
2022-03-11 01:44:07 +0900

    update) room router

--- routes/room.js
+++ routes/room.js
@@ -8,9 +8,9 @@ router.post('/room/userInfo', async (req,res) => {
     try {
         const { id } = req.body;
         const userInfo = await Users.findOne({ id }, { _id: false, id: true, state: true })
-        res.status(200).json(
+        res.status(200).json({
             userInfo
-        )
+        })
     } catch(error){
         res.status(400).json({ ok: false })
     }
