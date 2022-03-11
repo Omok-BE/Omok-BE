551fa08f2c18ad970ef7bee200bbdbb4dd8344b3
haksae90 <95196634+Haksae90@users.noreply.github.com>
2022-03-11 18:13:33 +0900

    test) game socket

--- socket.js
+++ socket.js
@@ -89,6 +89,7 @@ waitingRoom.on("connection", (socket) => {
       await Users.updateOne({ id: socket.nickname }, { $set: { state: "online" }})
       console.log("소켓방", socket.rooms)
       console.log(waitingRoom.adapter.rooms)
+      console.log(socket.id)
       if(socket.rooms.has("player")){
         const playerCnt = waitingRoomCount("player") -1
         console.log("퇴장시 플레이", playerCnt)
