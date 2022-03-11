e8d179806b35aeef7dbdbe6b88b1808d30c22917
haksae90 <95196634+Haksae90@users.noreply.github.com>
2022-03-11 22:15:01 +0900

    test) room socket

--- socket.js
+++ socket.js
@@ -91,15 +91,15 @@ waitingRoom.on("connection", (socket) => {
       console.log("소켓방", socket.rooms)
       console.log(waitingRoom.adapter.rooms)
       console.log(socket.id)
-      if(socket.rooms.has("player")){
+      // if(socket.rooms.has("player")){
         // const playerCnt = waitingRoomCount("player") -1
-        console.log("퇴장시 플레이", playerCnt)
+        // console.log("퇴장시 플레이", playerCnt)
         // await Rooms.updateOne({ roomNum: theRoomNumber }, { $set: { playerCnt }})
-      } else {
+      // } else {
         // const observerCnt = waitingRoomCount("observer") -1
-        console.log("퇴장시 관전자", observerCnt)
-        // await Rooms.updateOne({ roomNum: theRoomNumber }, { $set: { observerCnt }})
-      }} catch(error) {
+        // console.log("퇴장시 관전자", observerCnt)
+        // await Rooms.updateOne({ roomNum: theRoomNumber }, { $set: { observerCnt }})}
+      } catch(error) {
         console.log("errorMessage",error)
       }
     })
