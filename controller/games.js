6b9d8d18294d3107aec361f89d5c672336b86da2
haksae90 <95196634+Haksae90@users.noreply.github.com>
2022-03-12 17:02:51 +0900

    update) game create

--- controller/games.js
+++ controller/games.js
@@ -19,7 +19,6 @@ const gameCreate = async (req, res) => {
             whiteTeamPlayer,
             whiteTeamObserver
         });
-        await Rooms.deleteOne({ roomNum });
         res.status(201).json({ ok: true });
     } catch(error) {
         console.log(error)
