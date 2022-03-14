fbf99133ce0cdf368010ca2a5503e4bcd8b23e3d
haksae90 <95196634+Haksae90@users.noreply.github.com>
2022-03-15 01:54:20 +0900

    update) Room userInfo

--- controller/room.js
+++ controller/room.js
@@ -8,19 +8,10 @@ const userInfo = async (req,res) => {
         await Rooms.updateOne({ roomNum }, { $addToSet: { participants: id }})
         const userList = await Rooms.findOne({ roomNum }, { _id: false, participants: true })
         const userInfos = []
-        console.log(userList.participants)
         for (let element of userList.participants) {
             const userInfo = await Users.findOne({ id: element }, { _id: false, id: true, score: true, point: true, state: true })
             userInfos.push(userInfo)
-            console.log(userInfo)
-            console.log(userInfos)
         }
-        // userList.participants.forEach( async (element) => {
-        //     const userInfo = await Users.findOne({ id: element }, { _id: false, id: true, score: true, point: true, state: true })
-        //     console.log(userInfo)
-        //     userInfos.push(userInfo)
-        // });
-        console.log(userInfos)
         res.status(200).json({
             userInfos
         })
