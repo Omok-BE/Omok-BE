09d9f83d4f072d21e0c20e2ba0e9b4455a8d5202
haksae90 <95196634+Haksae90@users.noreply.github.com>
2022-03-17 13:41:43 +0900

    test) controller room. userInfos

--- controller/room.js
+++ controller/room.js
@@ -4,14 +4,39 @@ const Users = require('../models/users');
 // 대기실 입장시 유저 정보 전달
 const userInfo = async (req,res) => {
     try {
-        const { id, roomNum } = req.body;
-        await Rooms.updateOne({ roomNum }, { $addToSet: { participants: id }})
-        const userList = await Rooms.findOne({ roomNum }, { _id: false, participants: true })
-        const userInfos = []
-        for (let element of userList.participants) {
-            const userInfo = await Users.findOne({ id: element }, { _id: false, id: true, score: true, point: true, state: true })
-            userInfos.push(userInfo)
-        }
+        const { roomNum } = req.params;
+        const userInfos = await Rooms.aggregate([
+            {
+                $match: { roomNum: Number(roomNum) }                
+            },
+            {
+                $lookup:
+                {
+                    from: "users",
+                    localField: "blackTeamPlayer",
+                    foreignField: "id",
+                    pipeline: [
+                        { $project: { _id: 0, __v: 0, nickname: 0, pass: 0, teachingCnt: 0 } }
+                     ],
+                      as: "blackPlayerInfo"
+                }
+            },
+            {
+                $lookup:
+                {
+                    from: "users",
+                    localField: "whiteTeamPlayer",
+                    foreignField: "id",
+                    pipeline: [
+                        { $project: { _id: 0, __v: 0, nickname: 0, pass: 0, teachingCnt: 0 } }
+                     ],
+                      as: "whitePlayerInfo"
+                }
+            },
+            {
+                $project: { _id: 0, __v: 0, blackTeamObserver: 1, whiteTeamObserver: 1 }
+            }
+        ])
         res.status(200).json({
             userInfos
         })
