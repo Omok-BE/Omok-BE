8140d9d33cfb70d819b83ac650ad665132be21ac
haksae90 <95196634+Haksae90@users.noreply.github.com>
2022-03-17 14:43:56 +0900

    test) waitingRoom socket

--- routes/room.js
+++ routes/room.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const { userInfo } = require('../controller/room');
 
 // 대기실 입장시 유저 정보 전달
-router.post('/room/userInfo/:roomNum', userInfo);
+router.get('/room/userInfo/:roomNum', userInfo);
 
 module.exports = router;
