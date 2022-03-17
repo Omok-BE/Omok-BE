160609f350c66a9808953e5d1f7c4e9ed99de2f2
haksae90 <95196634+Haksae90@users.noreply.github.com>
2022-03-17 22:03:55 +0900

    Delete) room controller

--- routes/room.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const { userInfo } = require('../controller/room');
-
-// 대기실 입장시 유저 정보 전달
-router.get('/room/userInfo/:roomNum', userInfo);
-
-module.exports = router;
