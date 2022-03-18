df0ea0e4bc1982161755487814989de99579775f
haksae90 <95196634+Haksae90@users.noreply.github.com>
2022-03-18 21:56:37 +0900

    Test) MongoDB.aggregate

--- routes/games.js
+++ routes/games.js
@@ -38,7 +38,7 @@ router.get('/test/:roomNum', async (req,res) => {
           localField: 'blackTeamPlayer',
           foreignField: 'nickname',
           pipeline: [
-            { $project: { _id: 0 } }
+            { $project: { _id: 0, __v: 0, pass: 0 } }
           ],
           as: 'blackPlayerInfo',
         },
