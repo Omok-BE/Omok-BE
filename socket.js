498618782d748ace30159a74a205d8c67cbd6c25
suzy <2021234@naver.com>
2022-03-25 15:27:44 +0900

    update_socket-disconnecting_7

--- socket.js
+++ socket.js
@@ -664,7 +664,7 @@ gameRoom.on('connect', async (socket) => {
 
       gameRoom.to(thisgameNum).emit('bye', socket.id);
       const observerCnt = gameRoomCount(thisgameNum) - 3; //(-2 플레이어)+(-1 나가는 옵저버)
-      await Users.updateOne({ id:socket.nickname }, { $set: { state: 'online' }}); 
+     
       // console.log('게임방 소켓 퇴장observerCnt:', observerCnt);
       await Rooms.updateOne({ roomNum:thisgameNum }, { $set: { observerCnt } });
       console.log('게임방 퇴장 소켓 disconnecting🖐️🖐️');
