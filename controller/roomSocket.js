58a6ea16c5b5cc33b75627a564d1db9dabf2f221
haksae90 <95196634+Haksae90@users.noreply.github.com>
2022-03-29 04:02:19 +0900

    Refactor) roomSocket controller

--- controller/roomSocket.js
+++ controller/roomSocket.js
@@ -5,23 +5,23 @@ exports.nicknameEvent = function(socket){
     socket.on('nickname', (nickname) => (socket['nickname'] = nickname))
 };
 
-//플레이어로 입장시 정보 업데이트
-exports.enterRoomPlayer = function(socket){
-  socket.on('enterRoomPlayer', async (data) => {
-    const { roomNum, state } = data;
-    roomNumber = roomNum;
-    const role = `${roomNum}player`;
-    socket.join(roomNum);
-    socket.join(role);
-    const playerCnt = waitingRoomCount(role);
-    console.log("state", state)
-    await enterRoomByPlayer({
-      id: socket.nickname,
-      roomNum,
-      playerCnt,
-      state
-    });
-    const userInfos = await findUserInfos(roomNum);
-    waitingRoom.to(roomNum).emit('welcome', socket.nickname, userInfos);
-  });
-}
+// //플레이어로 입장시 정보 업데이트
+// exports.enterRoomPlayer = function(socket){
+//   socket.on('enterRoomPlayer', async (data) => {
+//     const { roomNum, state } = data;
+//     roomNumber = roomNum;
+//     const role = `${roomNum}player`;
+//     socket.join(roomNum);
+//     socket.join(role);
+//     const playerCnt = waitingRoomCount(role);
+//     console.log("state", state)
+//     await enterRoomByPlayer({
+//       id: socket.nickname,
+//       roomNum,
+//       playerCnt,
+//       state
+//     });
+//     const userInfos = await findUserInfos(roomNum);
+//     waitingRoom.to(roomNum).emit('welcome', socket.nickname, userInfos);
+//   });
+// };
--- socket/socket.js
+++ socket/socket.js
@@ -57,24 +57,23 @@ waitingRoom.on('connection', (socket) => {
   SocketEvent.nicknameEvent(socket);
 
   //플레이어로 입장시 정보 업데이트
-  SocketEvent.enterRoomPlayer(socket);
-  // socket.on('enterRoomPlayer', async (data) => {
-  //   const { roomNum, state } = data;
-  //   roomNumber = roomNum;
-  //   const role = `${roomNum}player`;
-  //   socket.join(roomNum);
-  //   socket.join(role);
-  //   const playerCnt = waitingRoomCount(role);
-  //   console.log("state", state)
-  //   await enterRoomByPlayer({
-  //     id: socket.nickname,
-  //     roomNum,
-  //     playerCnt,
-  //     state
-  //   });
-  //   const userInfos = await findUserInfos(roomNum);
-  //   waitingRoom.to(roomNum).emit('welcome', socket.nickname, userInfos);
-  // });
+  socket.on('enterRoomPlayer', async (data) => {
+    const { roomNum, state } = data;
+    roomNumber = roomNum;
+    const role = `${roomNum}player`;
+    socket.join(roomNum);
+    socket.join(role);
+    const playerCnt = waitingRoomCount(role);
+    console.log("state", state)
+    await enterRoomByPlayer({
+      id: socket.nickname,
+      roomNum,
+      playerCnt,
+      state
+    });
+    const userInfos = await findUserInfos(roomNum);
+    waitingRoom.to(roomNum).emit('welcome', socket.nickname, userInfos);
+  });
 
   //관전자로 입장시 정보 업데이트
   socket.on('enterRoomObserver', async (data) => {
