a29e0d99fdd679611ff63ae2bf2cf301fba659b1
suzy <2021234@naver.com>
2022-03-28 14:02:11 +0900

    update_games-model,controller

--- models/games.js
+++ models/games.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose');
 const gamesSchema = new mongoose.Schema({
   gameNum: Number,
   gameName: String,
-  black: Array,
-  white: Array,
   blackTeamPlayer: String,
   blackTeamObserver: Array,
   whiteTeamPlayer: String,
