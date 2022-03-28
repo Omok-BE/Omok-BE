61ac7639a008dd32396a22bebe2dbe5e6b00b582
haksae90 <95196634+Haksae90@users.noreply.github.com>
2022-03-28 13:47:31 +0900

    Test) roomSocket

--- socket/socket.js
+++ socket/socket.js
@@ -284,7 +284,7 @@ waitingRoom.on('connection', (socket) => {
     const chatData = { nickname: socket.nickname, chat };
     console.log("emitchat",chatData)
     console.log(socket.rooms)
-    console.log(waitingRoom.adapter)
+    console.log(waitingRoom.rooms)
     waitingRoom.to(roomNum).emit('chat', chatData);
   });
 
