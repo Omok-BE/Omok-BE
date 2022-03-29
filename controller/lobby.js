38126da1acadefbb96b868690bf1d03d2194451d
jableee <kkambo93@naver.com>
2022-03-29 22:03:36 +0900

    userlist edit

--- controller/lobby.js
+++ controller/lobby.js
@@ -21,7 +21,7 @@ const userList = async (req, res) => {
     // 내 상태 최신화
     let id = req.params.id
     await User.updateOne({id}, {$set : { connect: 'online' }})
-    const allUser = await User.find({ connect: { $ne: 'offline' } });
+    const allUser = await User.find({ connect: { $ne: 'offline' }}, {_id:0, pass:0, state:0, teachingCnt:0, connect:0});
 
     res.send(allUser);
   } catch (err) {
