4f439e6b071b18e31eda1cbfc218717dbcd2aa16
haksae90 <95196634+Haksae90@users.noreply.github.com>
2022-03-30 23:32:47 +0900

    Update) gameSocket observercnt

--- socket/socket.js
+++ socket/socket.js
@@ -466,7 +466,7 @@ gameRoom.on('connection', async (socket) => {
       gameRoom.to(gameNum).emit('bye', socket.id);
       const observerCnt = gameRoomCount(gameNum) - 2; //(-2 플레이어)+(-1 나가는 옵저버)
       // console.log('게임방 소켓 퇴장observerCnt:', observerCnt);
-      if (observerCnt) await Rooms.updateOne({ roomNum:gameNum }, { $set: { observerCnt } });
+      if (observerCnt > 0) await Rooms.updateOne({ roomNum:gameNum }, { $set: { observerCnt } });
       console.log('게임방 퇴장 소켓 disconnecting🖐️🖐️');
       // console.log('게임방 퇴장 소켓 id:', socket.id);
       // console.log('게임방 퇴장 소켓.id,gameNum:', socket.nickname);
