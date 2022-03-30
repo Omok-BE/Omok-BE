c426a912b9505b2f472cdd0f55ae9ac8dd68a30c
suzy <2021234@naver.com>
2022-03-31 01:55:29 +0900

    update_API-gameStart-boardColor_1

--- controller/games.js
+++ controller/games.js
@@ -45,9 +45,9 @@ const gameStart = async (req, res) => {
     console.log("45,gameStart,req.params:", req.params)
     //게임방내 유저 state별 정보
     const gameInfo = await gameUserInfo(gameNum);
-    let gameName = await Games.findOne({ gameNum },{ _id:0, gameNum:1, gameName:1 });  
+    const gameName = await Games.findOne({ gameNum },{ _id:0, gameNum:1, gameName:1 });  
     const findBoardColor = await Rooms.findOne({ roomNum:gameNum }, { _id:0, boardColor:1 }); 
-    gameName.boardColor = findBoardColor.boardColor
+    gameInfo.push(findBoardColor)
     // console.log("51,gameStart,findBoardColor:",findBoardColor)
     // console.log("52,gameStart,gameName:",gameName)
     res.status(200).json({
