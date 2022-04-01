68b6432aa6d143593e32f46ca6214626ef22fc48
suzy <2021234@naver.com>
2022-04-02 05:04:12 +0900

    update_app.js

--- app.js
+++ app.js
@@ -10,6 +10,20 @@ const lobbyRouter = require('./routes/lobby');
 const gameRouter = require('./routes/games');
 const adminRouter = require('./routes/admin')
 
+const requestMiddleware = (req, res, next) => {
+  console.log(
+    'Request URL:',
+    req.originalUrl,
+    ' - ',
+    new Date(+new Date() + 3240 * 10000)
+      .toISOString()
+      .replace('T', ' ')
+      .replace(/\..*/, '')
+  );
+  next();
+};
+
+
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.static('./views'));
