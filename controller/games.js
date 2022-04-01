bed8e344cc394de3e914a8de6e2f1d399b7aed00
suzy <2021234@naver.com>
2022-04-02 05:29:58 +0900

    restorarion_2

--- controller/games.js
+++ controller/games.js
@@ -1,4 +1,3 @@
-const _ = require('mongoose-sequence');
 const Games = require('../models/games');
 const Rooms = require('../models/rooms');
 const Users = require('../models/users');
@@ -372,6 +371,58 @@ const gameDelete = async (req, res) => {
     });
   }
 };
+
+//게임방내 유저 state별 정보
+async function gameUserInfo(gameNum) {
+  return await Games.aggregate([
+   {
+     $match: { gameNum: Number(gameNum) },
+   },
+   {
+     $lookup: {
+       from: 'users',
+       localField: 'blackTeamPlayer',
+       foreignField: 'id',
+       as: 'blackTeamPlayer',
+     },
+   },
+   {
+     $lookup: {
+       from: 'users',
+       localField: 'blackTeamObserver',
+       foreignField: 'id',
+       as: 'blackTeamObserver',
+     },
+   },
+   {
+     $lookup: {
+       from: 'users',
+       localField: 'whiteTeamPlayer',
+       foreignField: 'id',
+       as: 'whiteTeamPlayer',
+     },
+   },
+   {
+     $lookup: {
+       from: 'users',
+       localField: 'whiteTeamObserver',
+       foreignField: 'id',
+       as: 'whiteTeamObserver',
+     },
+   },
+   {
+     $project: {
+       _id: 0,
+       blackTeamPlayer: { id: 1, score: 1, point: 1, state: 1, profileImage:1 },
+       blackTeamObserver: { id: 1, score: 1, point: 1, state: 1, teachingCnt:1, profileImage:1 },
+       whiteTeamPlayer: { id: 1, score: 1, point: 1, state: 1, profileImage:1 },
+       whiteTeamObserver: { id: 1, score: 1, point: 1, state: 1, teachingCnt:1, profileImage:1 },
+       timer: 1
+     },
+   },
+ ]);
+}
+
 
 
 module.exports = {
