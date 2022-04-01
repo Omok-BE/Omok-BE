19c8edfd12fc0ec54646afceb52d5d9bdcbed2c1
jableee <kkambo93@naver.com>
2022-04-01 16:11:34 +0900

    js commit

--- models/bugReports.js
+++ models/bugReports.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
-const bugReport = new mongoose.Schema({
+const bugReportSchema = new mongoose.Schema({
   reportUser: Object,
   gameData: Object, //제보당시의 gameroom 정보 
   gameInfo: Object,
   content: String, // 간단 내용 
 });
 
-module.exports = mongoose.model('bugReport', boardsSchema);
+module.exports = mongoose.model('bugReport', bugReportSchema);
