6981f4aa1627108304d23779f33af2f941a6054d
suzy <84648177+suzyp0223@users.noreply.github.com>
2022-04-06 01:34:02 +0900

    Update app.js

--- app.js
+++ app.js
@@ -26,12 +26,12 @@ const adminRouter = require('./routes/admin');
 
 
 //스웨거 자동생성을 위한 코드
-const swaggerUi = require("swagger-ui-express");
+// const swaggerUi = require("swagger-ui-express");
 //스웨거 아웃풋파일 저장 위치
-const swaggerFile = require("./swagger-output.json");
+// const swaggerFile = require("./swagger-output.json");
 
 // app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerFile));
-app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerFile, { explorer:true }));
+// app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerFile, { explorer:true }));
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
