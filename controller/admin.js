cc26b7dea2d203c10793762414879da17bd8ef91
jableee <kkambo93@naver.com>
2022-04-06 02:30:07 +0900

    js sentry commit

--- controller/admin.js
+++ controller/admin.js
@@ -3,6 +3,7 @@ const Games = require('../models/games')
 const Rooms = require('../models/rooms')
 const jwt = require('jsonwebtoken');
 const crypto = require('crypto');
+const Sentry = require("@sentry/node");
 
 // 관리자 로그인
 const adminLogin = async (req, res) => {
@@ -35,6 +36,7 @@ const adminLogin = async (req, res) => {
             message: '관리자 로그인 성공'
         })
     } catch(error) {
+        Sentry.captureException(err);
         console.error(error);
         res.status(403).json({
             errorMessage: '접근 권한이 없습니다'
@@ -49,6 +51,7 @@ const getWaitingRooms = async (req, res) => {
             waitingRooms
         })
     } catch(error) {
+        Sentry.captureException(err);
         console.error(error)
         res.status(400).json({
             errorMessage: '데이터 불러오기에 실패했습니다.'
@@ -64,6 +67,7 @@ const deleteWaitingRoom = async (req, res) => {
             message: '삭제되었습니다.'
         })
     } catch(error) {
+        Sentry.captureException(err);
         console.error(error)
         res.status(400).json({
             errorMessage: '오류가 발생하였습니다. 대기실을 확인해주세요.'
@@ -78,6 +82,7 @@ const getGameRooms = async (req, res) => {
             gameRooms
         })
     } catch(error) {
+        Sentry.captureException(err);
         console.error(error)
         res.status(400).json({
             errorMessage: '데이터 불러오기에 실패했습니다.'
@@ -94,6 +99,7 @@ const deleteGameRoom = async (req, res) => {
             message: '삭제되었습니다.'
         })
     } catch(error) {
+        Sentry.captureException(err);
         console.error(error)
         res.status(400).json({
             errorMessage: '오류가 발생하였습니다. 대기실을 확인해주세요.'
@@ -108,6 +114,7 @@ const getUsers = async (req, res) => {
             userList
         })
     } catch(error) {
+        Sentry.captureException(err);
         console.error(error)
         res.status(400).json({
             errorMessage: '데이터 불러오기에 실패했습니다.'
@@ -124,6 +131,7 @@ const editUserPoint = async (req, res) => {
             message: '포인트가 수정되었습니다.'
         })
     } catch(error) {
+        Sentry.captureException(err);
         console.error(error)
         res.status(400).json({
             errorMessage: '오류가 발생하였습니다. 다시 시도해주세요.'
@@ -139,6 +147,7 @@ const deleteUser = async (req, res) => {
             message: '삭제되었습니다.'
         })
     } catch(error) {
+        Sentry.captureException(err);
         console.error(error)
         res.status(400).json({
             errorMessage: '오류가 발생하였습니다. 유저리스트를 확인해주세요.'
